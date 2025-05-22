5111f83dab61935af034a50e55681b3f0ca048d8
m0jik <ajmueller25@gmail.com>
2025-05-22 01:41:38 -0500

    Longest Palindromic Substring

--- leetCodeJava/src/Solution.java
+++ leetCodeJava/src/Solution.java
@@ -1,45 +1,37 @@
 class Solution {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        if (nums1.length == 0 && nums2.length == 0) {
-            return 0.0;
-        }
-        
-        if (nums1.length > nums2.length) {
-            return findMedianSortedArrays(nums2, nums1);
-        }
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) return "";
 
-        int m = nums1.length, n = nums2.length;
-        int low = 0, high = m;
+        int start = 0;
+        int end = 0;
 
-        while (low <= high) {
-            int i = (low + high) / 2;
-            int j = (m + n + 1) / 2 - i;
-
-            int leftA = (i == 0) ? Integer.MIN_VALUE : nums1[i - 1];
-            int rightA = (i == m) ? Integer.MAX_VALUE : nums1[i];
-            int leftB = (j == 0) ? Integer.MIN_VALUE : nums2[j - 1];
-            int rightB = (j == n) ? Integer.MAX_VALUE : nums2[j];
-
-            if (leftA <= rightB && leftB <= rightA) {
-                if ((m + n) % 2 == 1) {
-                    return Math.max(leftA, leftB);
-                } else {
-                    return (Math.max(leftA, leftB) + Math.min(rightA, rightB)) / 2.0;
-                }
-            } else if (leftA > rightB) {
-                high = i - 1;
-            } else {
-                low = i + 1;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandFromMiddle(s, i, i);
+            int len2 = expandFromMiddle(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - ((len - 1) / 2);
+                end = i + (len / 2);
             }
         }
 
-        return 0.0;
+        return s.substring(start, end + 1);
+    }
+
+    public int expandFromMiddle(String s, int left, int right) {
+        if (s == null || left > right) return 0;
+
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right ++;
+        }
+
+        return right - left - 1;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] nums1 = {1,2};
-        int[] nums2 = {3,4};
-        System.out.println(solution.findMedianSortedArrays(nums1, nums2));
+        String s = "babad";
+        System.out.println(solution.longestPalindrome(s));
     }
 }
